450e98bb2dfb30ae98485072be5b9d519a22b467
wilstermanz <wilstermanz@gmail.com>
2023-01-09 22:36:27 -0600

    maybe?

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -22,7 +22,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
             for value in mat2[i]:
                 mat3[i].append(value.copy())
 
-    return mat3
+    return mat3.copy()
 
 
 if __name__ == "__main__":
