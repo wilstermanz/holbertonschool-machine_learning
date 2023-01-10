d4bd7761dfcd52f681af784540d564758f70d3b3
wilstermanz <wilstermanz@gmail.com>
2023-01-09 22:46:02 -0600

    Removed bad check

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -14,19 +14,16 @@ def cat_matrices2D(mat1, mat2, axis=0):
             mat3.append(row.copy())
 
     if axis == 1:
-        if len(mat1[0]) != len(mat2):
-            return None
-        for row in mat1:
-            mat3.append(row.copy())
-        for i in range(len(mat3)):
+        for i in range(len(mat1)):
             for value in mat2[i]:
-                mat3[i].append(value.copy())
+                mat3.append(mat1[i].copy() + mat2[i].copy())
 
-    return mat3[:]
+    return mat3
 
 
 if __name__ == "__main__":
     """Main test"""
+
     mat1 = [[1, 2], [3, 4]]
     mat2 = [[5, 6]]
     mat3 = [[7], [8]]
