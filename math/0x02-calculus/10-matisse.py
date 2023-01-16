3577959e0f5086f7b39fd4fabc9301781ca24221
wilstermanz <wilstermanz@gmail.com>
2023-01-16 00:35:03 -0600

    Solution for task 10

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Contains poly_derivative() and main test"""
+
+
+def poly_derivative(poly):
+    if len(poly) > 1:
+        derivatives = poly[1:]
+        for index in range(1, len(derivatives)):
+            derivatives[index] *= index + 1
+        return derivatives
+    elif len(poly) == 1:
+        return [0]
+    else:
+        return None
+
+
+
+if __name__ == "__main__":
+    poly = [5, 3, 0, 1]
+    print(poly_derivative(poly))
