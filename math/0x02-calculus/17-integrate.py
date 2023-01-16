635242a4211c5d19e427817c1571c4b6db56ce95
wilstermanz <wilstermanz@gmail.com>
2023-01-16 01:21:59 -0600

    Added case for len(poly) == 1

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,6 +6,8 @@ def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
     if type(poly) is not list or type(C) not in [int, float] or len(poly) == 0:
         return None
+    if len(poly) == 1:
+        return [C]
     coefficients = [C, *poly]
     for i in range(1, len(poly) + 1):
         coefficients[i] /= i
