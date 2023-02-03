899606724dad9e1fa7eda884bde31273d284d359
wilstermanz <wilstermanz@gmail.com>
2023-02-03 10:13:34 -0600

    Solution for task 1

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Task 1 - Neuron"""
+
+import numpy as np
+
+
+class Neuron:
+    """defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """Initializes a new Neuron"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """W getter method"""
+        return self.__W
+
+    @property
+    def b(self):
+        """b getter method"""
+        return self.__b
+
+    @property
+    def A(self):
+        """A getter method"""
+        return self.__A
