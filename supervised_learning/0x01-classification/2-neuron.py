60014e3c195c2ae2ef217bdabbb4922b63308823
wilstermanz <wilstermanz@gmail.com>
2023-02-03 11:01:13 -0600

    this isn't working yet

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Task 2 - Neuron"""
+
+import numpy as np
+
+
+class Neuron:
+    """defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """Initializes a new Neuron"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """W getter method"""
+        return self.__W
+
+    @property
+    def b(self):
+        """b getter method"""
+        return self.__b
+
+    @property
+    def A(self):
+        """A getter method"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        e = 2.7182818285
+        n = np.matmul(self.__W, X)
+        self.__A = np.array(list(map(lambda x: 1 / (1 + e**(-x)), n)))
+        return self.__A
