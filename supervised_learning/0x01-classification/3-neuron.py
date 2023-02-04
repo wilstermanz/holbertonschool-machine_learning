07c8ba2a7445035190165fc52400e7e453296788
wilstermanz <wilstermanz@gmail.com>
2023-02-03 18:05:51 -0600

    Solution for task 3

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Task 3 - Neuron"""
+import numpy as np
+
+
+class Neuron:
+    """defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """Initializes a new Neuron"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """W getter method"""
+        return self.__W
+
+    @property
+    def b(self):
+        """b getter method"""
+        return self.__b
+
+    @property
+    def A(self):
+        """A getter method"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        n = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-n))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = np.shape(Y)[1]
+        return np.sum((-Y * np.log(A)) - ((1 - Y) * np.log(1.0000001 - A))) / m
