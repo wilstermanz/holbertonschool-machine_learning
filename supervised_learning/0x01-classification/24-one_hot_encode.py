8c6052c3118bd114cce0ea2f526518b0a9979b2f
Zach Wilsterman <100233764+wilstermanz@users.noreply.github.com>
2023-02-08 14:17:44 -0600

    Checks Y is np.ndarray

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -6,6 +6,8 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """converts a numeric label vector into a one-hot matrix"""
     try:
+        if type(Y) is not np.ndarray:
+            raise TypeError
         one_hot = np.zeros((classes, len(Y)))
         one_hot[Y, np.arange(len(Y))] = 1
         return one_hot
