f3b4bbd66575aec164f033cdfbe67da2799a2478
Zach Wilsterman <100233764+wilstermanz@users.noreply.github.com>
2023-02-08 14:14:37 -0600

    Solution for task 24

---
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 24 - One-Hot Encode"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+  """converts a numeric label vector into a one-hot matrix"""
+    try:
+        one_hot = np.zeros((classes, len(Y)))
+        one_hot[Y, np.arange(len(Y))] = 1
+        return one_hot
+    except Exception:
+        return None
