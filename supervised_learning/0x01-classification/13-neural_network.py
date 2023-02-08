c4561d2d988ff173a68d9a1046a09f569519cab7
Zach Wilsterman <100233764+wilstermanz@users.noreply.github.com>
2023-02-07 21:19:44 -0600

    Solution for task 13

---
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""Task 13 - NeuralNetwork Gradient Descent """
+import numpy as np
+
+
+class NeuralNetwork:
+    """defines a neural network with one hidden layer
+    performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        """intializes NeuralNetwork class"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """W1 getter"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """b1 getter"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """A1 getter"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """W2 getter"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """b2 getter"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """A2 getter"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of neural network"""
+        z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-z1))
+        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = np.shape(Y)[1]
+        return np.sum((-Y * np.log(A)) - ((1 - Y) * np.log(1.0000001 - A))) / m
+    
+    def evaluate(self, X, Y):
+        """Evaluates the neural network’s predictions"""
+        prediction = np.where(self.forward_prop(X)[1] >= .5, 1, 0)
+        return prediction, self.cost(Y, self.__A2)
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """Calculates one pass of gradient descent on the neural network"""
+        m = np.shape(Y)[1]
+        dz2 = A2 - Y
+        dW2 = np.matmul(dz2, A1.T) / m
+        db2 = np.sum(dz2, axis=1, keepdims=True) / m
+        dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
+        dW1 = np.matmul(dz1, X.T) / m
+        db1 = np.sum(dz1, axis=1, keepdims=True) / m
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
