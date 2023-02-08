080da3dca40687c21d3dd37792ca6ab1ebe602cd
Zach Wilsterman <100233764+wilstermanz@users.noreply.github.com>
2023-02-08 17:20:58 -0600

    Solution for task 25

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 25 - One-Hot decode"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """converts a one-hot matrix into a vector of labels"""
+    try:
+        if type(one_hot) is not np.ndarray:
+            raise TypeError
+        return np.argmax(one_hot.T, axis=1)
+    except Exception:
+        return None
