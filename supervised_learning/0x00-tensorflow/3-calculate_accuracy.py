ed61e25419df5bc6820b0c5482fe5ef07f57c86b
wilstermanz <wilstermanz@gmail.com>
2023-02-11 11:32:20 -0600

    Solution for task 3

--- supervised_learning/0x00-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x00-tensorflow/3-calculate_accuracy.py
@@ -5,4 +5,7 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """calculates the accuracy of a prediction"""
-    return tf.metrics.accuracy(y, y_pred)
+    answer = tf.math.argmax(y, axis=1)
+    prediction = tf.math.argmax(y_pred, axis=1)
+    equality = tf.math.equal(prediction, answer)
+    return tf.math.reduce_mean(tf.cast(equality, tf.float32))
