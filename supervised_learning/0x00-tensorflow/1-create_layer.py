c6e4a82a8a913f9ca02c276412566e7e1770e6ba
wilstermanz <wilstermanz@gmail.com>
2023-02-10 18:31:00 -0600

    Different name

--- supervised_learning/0x00-tensorflow/1-create_layer.py
+++ supervised_learning/0x00-tensorflow/1-create_layer.py
@@ -12,6 +12,5 @@ def create_layer(prev, n, activation):
         activation=activation,
         kernel_initializer=tf.contrib.layers.variance_scaling_initializer(
             mode="FAN_AVG"),
-        name='layer',
     )
     return layer
