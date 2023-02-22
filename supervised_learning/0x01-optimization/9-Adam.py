ec9a53eeb916e55cd83f34720be7ebd80a26b3a2
wilstermanz <wilstermanz@gmail.com>
2023-02-22 17:49:55 -0600

    Solution for task 9

---
+++ supervised_learning/0x01-optimization/9-Adam.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 9"""
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """updates a variable in place using the Adam optimization algorithm"""
+    new_v = beta1 * v + (grad * (1 - beta1))
+    new_s = beta2 * s + (grad**2 * (1 - beta2))
+    v_corrected = new_v / (1 - beta1**t)
+    s_corrected = new_s / (1 - beta2**t)
+    var = var - alpha * (v_corrected / np.sqrt(s_corrected + epsilon))
+    return var, new_v, new_s
