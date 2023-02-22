7676cff7f282cab70ff6d343d8fd62e85d2dc248
wilstermanz <wilstermanz@gmail.com>
2023-02-21 21:01:37 -0600

    Solution for task 3

---
+++ supervised_learning/0x01-optimization/3-mini_batch.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""Task 3"""
+import tensorflow as tf
+
+
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
+                     batch_size=32, epochs=5,
+                     load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
+    """trains a loaded neural network model using mini-batch gradient descent"""
+
+    def print_epoch(epochs, train_cost, train_accuracy,
+                   valid_cost, valid_accuracy):
+        """Prints epoch info"""
+        print("After {} epochs:\n".format(epochs) +
+              "\tTraining Cost: {}\n".format(train_cost) +
+              "\tTraining Accuracy: {}\n".format(train_accuracy) +
+              "\tValidation Cost: {}\n".format(valid_cost) +
+              "\tValidation Accuracy: {}".format(valid_accuracy))
+
+    def print_step(step_number, step_cost, step_accuracy):
+        """Prints step info"""
+        print("\tStep {}:\n".format(step_number) +
+              "\t\tCost: {}\n".format(step_cost) +
+              "\t\tAccuracy: {}".format(step_accuracy))
+
+    with tf.Session() as sess:
+        loader = tf.train.import_meta_graph(load_path + '.meta')
+        loader.restore(sess, load_path)
+        graph = tf.get_default_graph()
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        train_op = tf.get_collection('train_op')
+        num_batches = (len(X_train) // batch_size) + 1
+        last_batch_size = len(X_train) % batch_size
+
+        for epoch in range(epochs):
+            train_cost, train_accuracy = sess.run((loss, accuracy),
+                                                  feed_dict={x: X_train,
+                                                             y: Y_train})
+            valid_cost, valid_accuracy = sess.run((loss, accuracy),
+                                                  feed_dict={x: X_valid,
+                                                             y: Y_valid})
+            print_epoch(epoch, train_cost, train_accuracy,
+                        valid_cost, valid_accuracy)
+            X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
+            for step in range(num_batches):
+                if step < (num_batches - 1):
+                    X_batch = X_shuffled[step * batch_size:
+                                         (step + 1) * batch_size]
+                    Y_batch = Y_shuffled[step * batch_size:
+                                         (step + 1) * batch_size]
+                    sess.run(
+                        train_op,
+                        feed_dict={x: X_batch, y: Y_batch}
+                        )
+                if last_batch_size != 0 and step == num_batches - 1:
+                    X_batch = X_shuffled[step * last_batch_size:
+                                         (step + 1) * last_batch_size]
+                    Y_batch = Y_shuffled[step * last_batch_size:
+                                         (step + 1) * last_batch_size]
+                    sess.run(
+                        train_op,
+                        feed_dict={x: X_batch, y: Y_batch}
+                        )
+                if step > 0 and step % 100 == 0:
+                    step_cost, step_accuracy = sess.run(
+                        (loss, accuracy),
+                        feed_dict={x: X_batch, y: Y_batch}
+                        )
+                    print_step(step, step_cost, step_accuracy)
+        print_epoch(epochs, train_cost, train_accuracy,
+                    valid_cost, valid_accuracy)
+    saver = tf.train.Saver()
+    return saver.save(sess, save_path)
