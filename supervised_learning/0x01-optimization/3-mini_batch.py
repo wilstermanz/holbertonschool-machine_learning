13f4318c605670ebe5a43faf4ae2fc7bcdc059f9
wilstermanz <wilstermanz@gmail.com>
2023-02-21 21:46:17 -0600

    Fixed import

--- supervised_learning/0x01-optimization/3-mini_batch.py
+++ supervised_learning/0x01-optimization/3-mini_batch.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Task 3"""
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
