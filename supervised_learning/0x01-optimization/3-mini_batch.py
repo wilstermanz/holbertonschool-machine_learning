199c76a4ccf429292aa441dae700de1a7935f276
wilstermanz <wilstermanz@gmail.com>
2023-02-21 21:59:34 -0600

    a few changes

--- supervised_learning/0x01-optimization/3-mini_batch.py
+++ supervised_learning/0x01-optimization/3-mini_batch.py
@@ -37,10 +37,10 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
         accuracy = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
         train_op = tf.get_collection('train_op')
-        num_batches = (len(X_train) // batch_size) + 1
-        last_batch_size = len(X_train) % batch_size
-
-        for epoch in range(epochs):
+        num_batches = (len(X_train) // batch_size)
+        if num_batches % batch_size != 0:
+            num_batches += 1
+        for epoch in range(epochs + 1):
             train_cost, train_accuracy = sess.run((loss, accuracy),
                                                   feed_dict={x: X_train,
                                                              y: Y_train})
@@ -49,33 +49,23 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
                                                              y: Y_valid})
             print_epoch(epoch, train_cost, train_accuracy,
                         valid_cost, valid_accuracy)
-            X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
-            for step in range(num_batches):
-                if step < (num_batches - 1):
-                    X_batch = X_shuffled[step * batch_size:
-                                         (step + 1) * batch_size]
-                    Y_batch = Y_shuffled[step * batch_size:
-                                         (step + 1) * batch_size]
-                    sess.run(
-                        train_op,
-                        feed_dict={x: X_batch, y: Y_batch}
-                        )
-                if last_batch_size != 0 and step == num_batches - 1:
-                    X_batch = X_shuffled[step * last_batch_size:
-                                         (step + 1) * last_batch_size]
-                    Y_batch = Y_shuffled[step * last_batch_size:
-                                         (step + 1) * last_batch_size]
-                    sess.run(
-                        train_op,
-                        feed_dict={x: X_batch, y: Y_batch}
-                        )
-                if step > 0 and step % 100 == 0:
-                    step_cost, step_accuracy = sess.run(
-                        (loss, accuracy),
-                        feed_dict={x: X_batch, y: Y_batch}
-                        )
-                    print_step(step, step_cost, step_accuracy)
-        print_epoch(epochs, train_cost, train_accuracy,
-                    valid_cost, valid_accuracy)
+            if (epoch < epochs):
+                X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
+                for step in range(num_batches):
+                    if step < (num_batches - 1):
+                        X_batch = X_shuffled[step * batch_size:
+                                            (step + 1) * batch_size]
+                        Y_batch = Y_shuffled[step * batch_size:
+                                            (step + 1) * batch_size]
+                        sess.run(
+                            train_op,
+                            feed_dict={x: X_batch, y: Y_batch}
+                            )
+                    if step > 0 and (step + 1) % 100 == 0:
+                        step_cost, step_accuracy = sess.run(
+                            (loss, accuracy),
+                            feed_dict={x: X_batch, y: Y_batch}
+                            )
+                        print_step(step + 1, step_cost, step_accuracy)
         saver = tf.train.Saver()
         return saver.save(sess, save_path)
