f6f0c91a26e90ec45d08b3655ad1fa1a5ec58b97
wilstermanz <wilstermanz@gmail.com>
2023-02-22 18:55:36 -0600

    Solution for task 14

---
+++ supervised_learning/0x01-optimization/14-batch_norm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Task 14"""
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """
+    creates a batch normalization layer
+    for a neural network in tensorflow
+    """
+    layer = tf.layers.dense(
+        prev,
+        n,
+        activation=activation,
+        kernel_initializer=tf.contrib.layers.variance_scaling_initializer(
+            mode="FAN_AVG"),
+    )
+    mean, variance = tf.nn.moments(layer, 0)
+    gamma = tf.Variable(tf.ones(n), True)
+    beta = tf.Variable(tf.zeros(n), True)
+    epsilon = 10 ** -8
+    return tf.nn.batch_normalization(layer, mean, variance,
+                                     gamma, beta, epsilon)
