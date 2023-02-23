8882dbdcf1b82bb189cf0a8e94323a9ff2a19c9b
wilstermanz <wilstermanz@gmail.com>
2023-02-22 18:08:43 -0600

    Solution for task 11

---
+++ supervised_learning/0x01-optimization/11-learning_rate_decay.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Task 11"""
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """updates the learning rate using inverse time decay in numpy"""
+    return alpha / (1 + decay_rate * ( global_step // decay_step))
