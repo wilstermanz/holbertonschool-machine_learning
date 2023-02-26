64eecadef99b1dc4a0084d27f9e60d3c24ba3e4a
wilstermanz <wilstermanz@gmail.com>
2023-02-26 11:43:58 -0600

    Using L

--- supervised_learning/0x01-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x01-regularization/0-l2_reg_cost.py
@@ -15,5 +15,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
         * m is the number of data points used
         * Returns: the cost of the network accounting for L2 regularization
     """
-    weights2 = np.sum([np.sum(w ** 2) for k, w in weights.items() if 'W' in k])
+    weights2 = np.sum(
+        [np.sum(weights['W' + str(layer)] ** 2) for layer in range(1, L + 1)])
     return cost + ((lambtha / (2 * m) * weights2))
