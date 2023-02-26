0ceccb1dfefb5b18a425e3f88c75c79b42f65135
wilstermanz <wilstermanz@gmail.com>
2023-02-26 13:18:33 -0600

    Changed to correct v1 library calls

--- supervised_learning/0x01-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x01-regularization/3-l2_reg_create_layer.py
@@ -14,10 +14,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         * lambtha is the L2 regularization parameter
         * Returns: the output of the new layer
     """
-    # regularizer = tf.contrib.layers.l2_regularizer(lambtha)
-    # init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    regularizer = tf.keras.regularizers.l2(lambtha)
-    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    regularizer = tf.contrib.layers.l2_regularizer(lambtha)
+    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.Dense(n,
                             activation=activation,
                             kernel_initializer=initializer,
