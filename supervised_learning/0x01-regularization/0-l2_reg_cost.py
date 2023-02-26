9f75be73d0e2b2c2b946bd7f323518e2a0f70471
wilstermanz <wilstermanz@gmail.com>
2023-02-26 11:40:28 -0600

    Solution for task 0

---
+++ supervised_learning/0x01-regularization/0-l2_reg_cost.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Task 0"""
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """
+    calculates the cost of a neural network with L2 regularization:
+
+        * cost is the cost of the network without L2 regularization
+        * lambtha is the regularization parameter
+        * weights is a dictionary of the weights and biases (numpy.ndarrays) of
+          the neural network
+        * L is the number of layers in the neural network
+        * m is the number of data points used
+        * Returns: the cost of the network accounting for L2 regularization
+    """
+    weights2 = np.sum([np.sum(w ** 2) for k, w in weights.items() if 'W' in k])
+    return cost + ((lambtha / (2 * m) * weights2))
---
+++ supervised_learning/0x01-regularization/README.md
@@ -0,0 +1,4 @@
+# supervised_learning/0x01-regularization
+This repository contains projects for the regularization project  
+Author: Zach Wilsterman  
+02/26/2023
