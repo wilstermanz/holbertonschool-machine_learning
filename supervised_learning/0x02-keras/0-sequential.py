18edb83f3d37a107fdd93b213263edf31ef84984
wilstermanz <wilstermanz@gmail.com>
2023-03-04 13:52:30 -0600

    pycodestyle

--- supervised_learning/0x02-keras/0-sequential.py
+++ supervised_learning/0x02-keras/0-sequential.py
@@ -28,8 +28,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     for i in range(len(layers)):
         model.add(K.layers.Dense(layers[i], activation=activations[i],
                                  kernel_regularizer=K.regularizers.l2(lambtha),
-                                )
-                 )
+                                 )
+                  )
         if i != len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
 
