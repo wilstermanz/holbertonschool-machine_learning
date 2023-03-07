1b4c2afd51b1d654c491a16f19be7e599501c647
wilstermanz <wilstermanz@gmail.com>
2023-03-06 18:12:54 -0600

    Solution for task 9

---
+++ supervised_learning/0x02-keras/9-model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Task 9"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+    saves an entire model:
+
+        * network is the model to save
+        * filename is the path of the file that the model should be saved to
+        * Returns: None
+    """
+    network.save(filename)
+
+
+def load_model(filename):
+    """
+    loads an entire model:
+
+        * filename is the path of the file that the model should be loaded from
+        * Returns: the loaded model
+    """
+    K.models.load_model(filename)
