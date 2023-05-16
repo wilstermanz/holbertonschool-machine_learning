a523973f91814a79b523bd90b4b6feac3cdede5b
wilstermanz <wilstermanz@gmail.com>
2023-05-16 16:08:53 -0500

    Solution for task 3

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -25,3 +25,29 @@ class MultiNormal:
         self.mean = np.expand_dims(np.mean(data, axis=1), axis=1)
         self.cov = np.matmul((data - self.mean), (data - self.mean).T) \
             / (data.shape[1] - 1)
+
+    def pdf(self, x):
+        """
+        calculates the PDF at a data point:
+
+        x is a numpy.ndarray of shape (d, 1) containing the data point whose
+          PDF should be calculated
+            d is the number of dimensions of the Multinomial instance
+        If x is not a numpy.ndarray, raise a TypeError with the message x must
+          be a numpy.ndarray
+        If x is not of shape (d, 1), raise a ValueError with the message x must
+          have the shape ({d}, 1)
+        Returns the value of the PDF
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError('x must be a numpy.ndarray')
+        if len(x.shape) != 2 or x.shape[1] != 1:
+            raise ValueError(
+                'x must have the shape ({}, 1)'.format(x.shape[0]))
+
+        p1 = (2 * np.pi) ** (-x.shape[0] / 2)
+        p2 = np.linalg.det(self.cov) ** (-0.5)
+        p3 = np.matmul((x - self.mean).T, np.linalg.inv(self.cov))
+        pdf = p1 * p2 * np.exp(-0.5 * np.matmul(p3, (x - self.mean)))
+
+        return pdf
