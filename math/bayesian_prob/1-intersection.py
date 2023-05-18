54e9676995ae26c05ef994e131bb3d23233aac7a
wilstermanz <wilstermanz@gmail.com>
2023-05-17 21:00:14 -0500

    fixed shape check

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -43,7 +43,7 @@ def intersection(x, n, P, Pr):
         raise ValueError('x cannot be greater than n')
     if type(P) is not np.ndarray or P.ndim > 1:
         raise TypeError('P must be a 1D numpy.ndarray')
-    if type(Pr) is not np.ndarray or Pr.ndim != P.ndim:
+    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
         raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
     if np.max(P) > 1 or np.min(P) < 0:
         raise ValueError('All values in P must be in the range [0, 1]')
