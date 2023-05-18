18a2c001656d3120301fb6ce9800a89237286245
wilstermanz <wilstermanz@gmail.com>
2023-05-17 20:41:16 -0500

    Fixed dimension check

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -32,7 +32,7 @@ def likelihood(x, n, P):
             'x must be an integer that is greater than or equal to 0')
     if x > n:
         raise ValueError('x cannot be greater than n')
-    if type(P) is not np.ndarray or P.size > 1:
+    if type(P) is not np.ndarray or P.ndim > 1:
         raise TypeError('P must be a 1D numpy.ndarray')
     if np.where(P < 0)[0].size > 0 or np.where(P > 1)[0].size > 0:
         raise ValueError('All values in P must be in the range [0, 1]')
@@ -40,3 +40,8 @@ def likelihood(x, n, P):
     bincoef = np.math.factorial(n) / (
         np.math.factorial(x) * np.math.factorial(n - x))
     return bincoef * (P ** x) * ((1 - P) ** (n - x))
+
+
+if __name__ == '__main__':
+    P = np.linspace(0, 1, 11)
+    print(likelihood(26, 130, P))
