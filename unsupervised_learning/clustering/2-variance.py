2a645f152c63ffd2eefe8c3a7f264bb854dc6f93
wilstermanz <wilstermanz@gmail.com>
2023-05-31 21:30:04 -0500

    Solution for task 2

---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Task 2 - Variance"""
+import numpy as np
+
+
+def variance(X, C):
+    """
+    calculates the total intra-cluster variance for a data set:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    C is a numpy.ndarray of shape (k, d) containing the centroid means for
+    each cluster
+    You are not allowed to use any loops
+    Returns: var, or None on failure
+        var is the total variance
+    """
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None
+    if type(C) is not np.ndarray or X.ndim != 2:
+        return None
+    if X.shape[1] != C.shape[1]:
+        return None
+
+    distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+    minimums = np.min(distances, axis=1)
+    variance = np.sum(np.square(minimums))
+
+    return variance
