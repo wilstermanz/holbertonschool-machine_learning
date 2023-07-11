c0d72564fe0a3fb4d2cf1570b6f86d96177b9abb
wilstermanz <wilstermanz@gmail.com>
2023-07-10 20:04:14 -0500

    Solution for task 0

---
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Task 0"""
+import numpy as np
+
+
+class RNNCell:
+    """represents a cell of a simple RNN"""
+
+    def __init__(self, i, h, o):
+        """
+        Inputs:
+            i: the dimensionality of the data
+            h: the dimensionality of the hidden state
+            o: the dimensionality of the outputs
+
+        Creates the public instance attributes Wh, Wy, bh, by that represent
+        the weights and biases of the cell
+            Wh and bh are for the concatenated hidden state and input data
+            Wy and by are for the output
+        The weights should be initialized using a random normal distribution
+        in the order listed above
+        The weights will be used on the right side for matrix multiplication
+        The biases should be initialized as zeros
+        """
+        self.Wh = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h, o)
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        Inputs:
+            x_t: a numpy.ndarray of shape (m, i) that contains the data input
+            for the cell
+                m is the batch size for the data
+            h_prev: a numpy.ndarray of shape (m, h) containing the previous
+            hidden state
+
+        The output of the cell should use a softmax activation function
+
+        Returns:
+            h_next: the next hidden state
+            y: the output of the cell
+        """
+        def softmax(x):
+            return np.exp(x - np.max(x)) / np.exp(x - np.max(x)).sum()
+
+        Whh = self.Wh[:h_prev.shape[1], :]
+        Wxh = self.Wh[h_prev.shape[1]:, :]
+        h_t = np.tanh((Whh @ h_prev) + (Wxh @ x_t) + self.bh)
+        y_t = (self.Wy @ h_t) + self.by
+
+        return h_t, softmax(y_t)
---
+++ supervised_learning/RNNs/README.md
@@ -0,0 +1,1 @@
+A readme for the RNN project
