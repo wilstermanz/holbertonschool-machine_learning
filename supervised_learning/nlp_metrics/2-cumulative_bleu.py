dcbfe831ba23e754e54fff7ca5b35dd6792bc81f
wilstermanz <wilstermanz@gmail.com>
2023-08-07 14:44:04 -0500

    Fixed count_ngrams

--- supervised_learning/nlp_metrics/2-cumulative_bleu.py
+++ supervised_learning/nlp_metrics/2-cumulative_bleu.py
@@ -21,9 +21,9 @@ def cumulative_bleu(references, sentence, N):
         for i in range(0, len(sentence) - n + 1):
             grams = [sentence[i + j] for j in range(n)]
             ngram = ''
-            for word in grams:
+            for j, word in enumerate(grams):
                 ngram += word
-                if word != grams[-1]:
+                if j != n - 1:
                     ngram += ' '
             ngrams.append(ngram)
 
@@ -32,8 +32,8 @@ def cumulative_bleu(references, sentence, N):
     def count_ngrams(sentence, ngrams, n):
         ngram_sentence = make_ngrams(sentence, n)
         ngram_count = {ngram: 0 for ngram in ngrams}
-        for ngram in ngrams:
-            if ngram in ngram_sentence:
+        for ngram in ngram_sentence:
+            if ngram in ngrams:
                 ngram_count[ngram] += 1
 
         return ngram_count
@@ -41,7 +41,7 @@ def cumulative_bleu(references, sentence, N):
     Pn = []
     for n in range(1, N + 1):
         ngrams = make_ngrams(sentence, n)
-
+    
         c = len(sentence)
         r = min([len(reference) for reference in references])
         BP = 1 if c > r else np.exp(1 - (r / c))
@@ -51,10 +51,13 @@ def cumulative_bleu(references, sentence, N):
             ngram_count = count_ngrams(reference, ngrams, n)
             for ngram in ngrams:
                 max_ref_count[ngram] = max(ngram_count[ngram],
-                                           max_ref_count[ngram])
+                                        max_ref_count[ngram])
 
         Pn.append(np.sum(list(max_ref_count.values())) / np.sum(
             list(count_ngrams(sentence, ngrams, n).values())))
+        for value in Pn:
+            if value == 0:
+                Pn.remove(value)
 
     bleu = BP * np.exp(np.sum((1/N) * np.log(Pn)))
 
