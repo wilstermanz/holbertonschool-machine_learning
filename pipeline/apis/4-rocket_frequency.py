fca6744ac4358191eb563c857dce5e425f64828f
wilstermanz <wilstermanz@gmail.com>
2023-10-18 17:17:28 -0500

    Made some changes for clarity

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -3,28 +3,28 @@
 
 if __name__ == '__main__':
     from requests import get
-    API = 'https://api.spacexdata.com/'
-    LAUNCHES = 'v4/launches/'
-    ROCKETS = 'v4/rockets/'
 
-    rockets_count = {}
-    rocket_names = {}
+    API = 'https://api.spacexdata.com/v4/'
 
-    launches = get(API + LAUNCHES).json()
+    rocket_names = {}   # (rocket ID: rocket name)
+    rockets_count = {}  # {rocket name: count}
+
+    launches = get(API + 'launches/').json()
     for launch in launches:
+        rocket_id = launch['rocket']
 
         # get rocket name from dictionary, else call API
         try:
-            name = rocket_names[launch['rocket']]
-            rockets_count[name] += 1    # add to count
+            name = rocket_names[rocket_id]  # get name from dict
+            rockets_count[name] += 1        # add to count
 
         except KeyError:
-            # get rocket name from API
-            name = get(API + ROCKETS + launch['rocket']).json()['name']
+            # get name from API
+            name = get(API + 'rockets/' + rocket_id).json()['name']
 
             # add name to dict
-            rocket_names[launch['rocket']] = name
-            rockets_count[name] = 1     # create dict entry and set to 1
+            rocket_names[rocket_id] = name
+            rockets_count[name] = 1         # create dict entry and set to 1
 
     # sort alphabetically into list of tuples
     rockets_count = sorted(rockets_count.items(), key=lambda d: d[0])
