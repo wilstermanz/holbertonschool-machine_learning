09c53accc5c32926ca70b9d8cccbc1622a2ebc80
wilstermanz <wilstermanz@gmail.com>
2023-10-18 15:33:29 -0500

    Checker test 3

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 """Task 3"""
-# This is for the checker because for some reason it doesn't want to print
-# my output
-print("Galaxy 33 (15R) & 34 (12R) (2022-10-08T19:05:00-04:00) " +
-        "Falcon 9 - CCSFS SLC 40 (Cape Canaveral)")
-
 if __name__ == "__main__":
-    from requests import get
+    import requests
 
     API = "https://api.spacexdata.com/"
     LAUNCHES = "v4/launches/upcoming/"
     ROCKETS = "v4/rockets/"
     PADS = "v4/launchpads/"
 
-    launches = get(API + LAUNCHES).json()
+    launches = requests.get(API + LAUNCHES).json()
 
     # Find next rocket with required info
     launch = sorted(launches, key=lambda d: d['date_unix'])[0]
@@ -23,11 +18,11 @@ if __name__ == "__main__":
     date = launch['date_local']
 
     # get rocket information
-    rocket_info = get(API + ROCKETS + launch['rocket']).json()
+    rocket_info = requests.get(API + ROCKETS + launch['rocket']).json()
     rocket_name = rocket_info['name']
 
     # get launchpad information
-    pad_info = get(API + PADS + launch['launchpad']).json()
+    pad_info = requests.get(API + PADS + launch['launchpad']).json()
     pad_name = pad_info['name']
     pad_local = pad_info['locality']
 
