2916c9650bdac285f0ae09f10e4eb5587b375de9
wilstermanz <wilstermanz@gmail.com>
2023-10-18 15:36:38 -0500

    Added execute permissions to file

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """Task 3"""
-if __name__ == "__main__":
-    print("HELLLLOOOOOOO!!!!!")
-    import requests
+
+if __name__ == '__main__':
+    from requests import get
 
     API = "https://api.spacexdata.com/"
     LAUNCHES = "v4/launches/upcoming/"
     ROCKETS = "v4/rockets/"
     PADS = "v4/launchpads/"
 
-    launches = requests.get(API + LAUNCHES).json()
+    launches = get(API + LAUNCHES).json()
 
     # Find next rocket with required info
     launch = sorted(launches, key=lambda d: d['date_unix'])[0]
@@ -19,11 +19,11 @@ if __name__ == "__main__":
     date = launch['date_local']
 
     # get rocket information
-    rocket_info = requests.get(API + ROCKETS + launch['rocket']).json()
+    rocket_info = get(API + ROCKETS + launch['rocket']).json()
     rocket_name = rocket_info['name']
 
     # get launchpad information
-    pad_info = requests.get(API + PADS + launch['launchpad']).json()
+    pad_info = get(API + PADS + launch['launchpad']).json()
     pad_name = pad_info['name']
     pad_local = pad_info['locality']
 
