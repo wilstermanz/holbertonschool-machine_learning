ff87ce7af5f2e1876971aa8e9fb79fc9012e6ba7
wilstermanz <wilstermanz@gmail.com>
2023-10-18 14:28:46 -0500

    Solution for task 2

---
+++ pipeline/apis/2-user_location.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Task 2"""
+import requests
+from sys import argv
+import time
+
+
+if __name__ == "__main__":
+    try:
+        r = requests.get(argv[1])
+
+        # if user not found
+        if r.status_code == 404:
+            print('not found')
+
+        # print remaining time if rate limit exceeded
+        if r.status_code == 403:
+            now = int(time.time())
+            reset = int(r.headers['X-RateLimit-Reset'])
+            diff = reset - now
+            print("Reset in {} min".format(diff // 60))
+
+        # print location if no error
+        if r.status_code == 200:
+            print(r.json()['location'])
+
+    # Print exception if incorrect URL is passed         
+    except requests.exceptions.MissingSchema as ms:
+        print(ms)
