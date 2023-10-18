7ff39a4744fc38334c711a8e807f4190642adbe9
wilstermanz <wilstermanz@gmail.com>
2023-10-18 16:19:56 -0500

    Changed to work with API v4

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -3,19 +3,32 @@
 
 if __name__ == '__main__':
     from requests import get
+    API = 'https://api.spacexdata.com/'
+    LAUNCHES = 'v4/launches/'
+    ROCKETS = 'v4/rockets/'
 
-    API = 'https://api.spacexdata.com/v3/launches'
-    rockets = {}
+    rockets_count = {}
+    rocket_names = {}
 
-    launches = get(API).json()
-
+    launches = get(API + LAUNCHES).json()
     for launch in launches:
+
+        # get rocket name from dictionary, else call API
         try:
-            rockets[launch['rocket']['rocket_name']] += 1
+            name = rocket_names[launch['rocket']]
+            rockets_count[name] += 1    # add to count
+
         except KeyError:
-            rockets[launch['rocket']['rocket_name']] = 1
+            # get rocket name from API
+            name = get(API + ROCKETS + launch['rocket']).json()['name']
 
-    rockets = sorted(rockets.items(), key=lambda d: d[0])
+            # add name to dict
+            rocket_names[launch['rocket']] = name
+            rockets_count[name] = 1     # create dict entry and set to 1
 
-    for rocket in sorted(rockets, key=lambda d: d[1], reverse=True):
+    # sort alphabetically into list of tuples
+    rockets_count = sorted(rockets_count.items(), key=lambda d: d[0])
+
+    # sort list by value and print
+    for rocket in sorted(rockets_count, key=lambda d: d[1], reverse=True):
         print('{}: {}'.format(rocket[0], rocket[1]))
