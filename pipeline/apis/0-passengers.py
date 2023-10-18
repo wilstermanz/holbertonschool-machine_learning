65145d1a79cb151089c0ac5307bbb4b239a94d50
wilstermanz <wilstermanz@gmail.com>
2023-10-18 13:10:03 -0500

    Solution for task 0

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Task 0"""
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    Using the Swapi API, returns the list of ships that can hold a given
+    number of passengers
+    """
+    page = 1
+    api = "https://swapi-api.hbtn.io/api/starships/"
+    ships = []
+
+    while True:
+        r = requests.get(api + f"?page={page}").json()
+
+        for ship in r['results']:
+            passengers = ship['passengers']
+            try:
+                if int(passengers.replace(',', '')) >= passengerCount:
+                    ships.append(ship['name'])
+            except ValueError:
+                pass
+
+        if not r['next']:
+            break
+
+        page += 1
+
+    return ships
+
+
+if __name__ == "__main__":
+    ships = availableShips(4)
+    for ship in ships:
+        print(ship)
