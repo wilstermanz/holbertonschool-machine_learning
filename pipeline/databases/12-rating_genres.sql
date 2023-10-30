49f5fb013a47d40c6e79bd53f35b09b8d5ff3a4b
wilstermanz <wilstermanz@gmail.com>
2023-10-30 13:16:08 -0500

    Pushed file for correct task

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,7 +1,9 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_show_ratings
-ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY title
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
