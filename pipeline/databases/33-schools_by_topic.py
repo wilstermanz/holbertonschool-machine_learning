5a68f29d48d677cd38037cc1f760fd204629ec7e
wilstermanz <wilstermanz@gmail.com>
2023-11-01 13:51:24 -0500

    Added documentation

--- pipeline/databases/33-schools_by_topic.py
+++ pipeline/databases/33-schools_by_topic.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ Task 33 """
 
+
 def schools_by_topic(mongo_collection, topic):
+    """ returns the list of school having a specific topic """
     return mongo_collection.find({"topics": topic})
